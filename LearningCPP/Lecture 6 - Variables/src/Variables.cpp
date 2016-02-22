a66cc1b159ad909f0126a52f0eda23768f5f65d6
Blake Barnes <ellisbarnes00@gmail.com>
2016-02-21 23:26:51 -0400

    LearningCPP: Lecture 6 - Variables

---
+++ LearningCPP/Lecture 6 - Variables/src/Variables.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main() {
+	int numberOfCats = 6; //Initialized and declared the number
+	int numberOfDogs = 8;
+	int totalAnimals = numberOfCats + numberOfDogs;
+
+	cout << "Number of Cats: " << numberOfCats << "\n";
+	cout << "Number of Dogs: " << numberOfDogs << "\n";
+	cout << "Number of Animals: " << totalAnimals << "\n\n";
+
+	cout << "A new dog was found! \n";
+
+	numberOfDogs = numberOfDogs + 1; //This adds one to the original amound of dogs
+	cout << "Number of Dogs: " << numberOfDogs << "\n";
+	cout << "Number of Animals: " << totalAnimals << "\n";
+
+	cin.get();
+	return 0;
+}

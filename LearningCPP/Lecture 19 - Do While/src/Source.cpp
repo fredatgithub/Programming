46fe82d4c0fdf23a942c2f922700063b99692557
BlakeBarnes00 <ellisbarnes00@gmail.com>
2016-02-28 01:09:49 -0400

    LearningCPP: Lecture 19 - Do While

--- LearningCPP/Lecture 19 - Do While/src/Source.cpp
+++ LearningCPP/Lecture 19 - Do While/src/Source.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
+#include <string>
 
 using std::cin;
 using std::cout;
 using std::endl;
+using std::string;
 
 
 int main() {
+	const string password = "hello";
+	int chances = 3;
+	string input;
 
+	do {
+		cout << "Enter your password: ";
+		cin >> input;
+		
+		if (input != password) {
+			chances--; //If you enter the wrong password then remove a chance
+			cout << "Incorrect password. ";
+			cout << "Chances left: " << chances << endl;
+			cin.get();
+		}
+		else if (input == password) {
+			cout << "Welcome!";
+			cin.get();
+		}
+	} while (input != password && chances > 0); //If your chances haven't ran out then ask for the password again
+	
+	if (chances == 0) {
+		cout << "\n\nNo more chances... Exiting" << endl; 
+		cin.get();
+	}
+	
+	return 0;
 }

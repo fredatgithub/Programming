9174e6debd14867a3f4f26d200c7ea39b08468c5
BlakeBarnes00 <ellisbarnes00@gmail.com>
2016-02-28 01:25:10 -0400

    Changed to strictly using a batch file

--- LearningCPP/Git Bot/src/Bot.cpp
+++ LearningCPP/Git Bot/src/Bot.cpp
@@ -1,17 +1,20 @@
 //This program requires that all you do is have http://git-scm.com/ installed and install with "Use from the Windows Command Prompt"
 
+// writing on a text file
 #include <iostream>
+#include <fstream>
 
-using std::cin;
-using std::cout;
-
-int main() {	
-	//Git Stuff
-	system("git add -A");
-	system("git commit -a");
-	system("git push");
-	
-	cout << "Thank you for using the bot :D";
-	cin.get();
+int main() {
+	std::ofstream myfile("Git.sh");
+	if (myfile.is_open())
+	{
+		myfile << "git add -A\n";
+		myfile << "git commit -a\n";
+		myfile << "git push\n";
+		myfile.close();
+	}
+	else {
+		std::cout << "Unable to open file";
+	}
 	return 0;
 }

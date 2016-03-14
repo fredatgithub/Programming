54d3e9b2d358ff9308bdc1574c845a7b533a49a7
BlakeBarnes00 <ellisbarnes00@gmail.com>
2016-03-13 23:44:00 -0300

    Created a "for loop" that would restart..

--- LearningCPP/Lecture 20 - For Loops/src/Source.cpp
+++ LearningCPP/Lecture 20 - For Loops/src/Source.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 
+using std::cout;
+using std::endl;
+
 int main() {
 
+
+
+	for (int value = 100; value > 0; value--) {
+
+		cout << " Value: \t" << value << endl;
+
+		if (value == 1) {
+			value = 100;
+		}
+	}
+
+	return 0;
 }

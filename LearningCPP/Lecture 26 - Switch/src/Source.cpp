7a770d67001980cad8757cbd770a3092cb24c16d
BlakeBarnes00 <ellisbarnes00@gmail.com>
2016-03-16 20:24:28 -0300

    Made a McDonalds menu using Switch statements and Arrays
    Signed-off-by: BlakeBarnes00 <ellisbarnes00@gmail.com>

---
+++ LearningCPP/Lecture 26 - Switch/src/Source.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	// The food menu
+	const int row = 10;
+	const int col = 3;
+	std::string foodMenu[row][col] = {
+		{ "01. McDouble",				"\t\t$1.00 + Tax",			"\t|" },
+		{ "02. McChicken",				"\t\t$1.00 + Tax",			"\t|" },
+		{ "03. Parfait",				"\t\t$1.00 + Tax",			"\t|" },
+		{ "04. Side Salad",				"\t\t$1.00 + Tax",			"\t|" },
+		{ "05. Small Fries",			"\t\t$1.00 + Tax",			"\t|" },
+		{ "06. Small Soft Drink",		"\t$1.00 + Tax",			"\t|" },
+		{ "07. 2 Pies",					"\t\t$1.00 + Tax",			"\t|" },
+		{ "08. Sundae",					"\t\t$1.00 + Tax",			"\t|" },
+		{ "09. Apple Dippers",			"\t$1.00 + Tax",			"\t|" },
+		{ "10. 3 Cookies",				"\t\t$1.00 + Tax",			"\t|" }
+	};
+
+	// Printing the menu
+	std::cout << "McDonalds Menu: " << std::endl;
+	std::cout << "=========================================================" << std::endl;
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			std::cout << foodMenu[i][j] << "\t";
+		}
+		std::cout << std::endl;
+	}
+	std::cout << "=========================================================" << std::endl;
+	
+	std::cout << "Welcome to McDonalds, what would you like to order? ";
+	std::cout << "Enter your choice: ";
+	
+	int order;
+	std::cin >> order;
+
+	switch (order){
+	case 1:
+		std::cout << "You ordered a McDouble." << std::endl;
+		std::cout << "Balance Due: $1.00 + Tax" << std::endl;
+		break;
+	case 2:
+		std::cout << "You ordered a McChicken." << std::endl;
+		std::cout << "Balance Due: $1.00 + Tax" << std::endl;
+		break;
+	case 3:
+		std::cout << "You ordered a Parfait." << std::endl;
+		std::cout << "Balance Due: $1.00 + Tax" << std::endl;
+		break;
+	case 4:
+		std::cout << "You ordered a Side Salad." << std::endl;
+		std::cout << "Balance Due: $1.00 + Tax" << std::endl;
+		break;
+	case 5:
+		std::cout << "You ordered a Small Fries." << std::endl;
+		std::cout << "Balance Due: $1.00 + Tax" << std::endl;
+		break;
+	case 6:
+		std::cout << "You ordered a Small Soft Drink." << std::endl;
+		std::cout << "Balance Due: $1.00 + Tax" << std::endl;
+		break;
+	case 7:
+		std::cout << "You ordered a 2 Pies." << std::endl;
+		std::cout << "Balance Due: $1.00 + Tax" << std::endl;
+		break;
+	case 8:
+		std::cout << "You ordered a Sundae." << std::endl;
+		std::cout << "Balance Due: $1.00 + Tax" << std::endl;
+		break;
+	case 9:
+		std::cout << "You ordered a Apple Dippers." << std::endl;
+		std::cout << "Balance Due: $1.00 + Tax" << std::endl;
+		break;
+	case 10:
+		std::cout << "You ordered a 3 Cookies." << std::endl;
+		std::cout << "Balance Due: $1.00 + Tax" << std::endl;
+		break;
+	default:
+		std::cout << "Unrecognized Order." << std::endl;
+		break;
+	}
+	std::cout << std::endl;
+
+	std::cout << "Thank you for choosing McDonalds, have a nice day!" << std::endl;
+	std::cin.get();
+	std::cin.get();
+	return 0;
+}

1228fd232d2ee9c24c821f967dd3d485fd344ad1
BlakeBarnes00 <ellisbarnes00@gmail.com>
2016-03-19 16:09:57 -0300

    Learned about Headers and Prototypes. Made a Pythagorean Theorem calculator
    Signed-off-by: BlakeBarnes00 <ellisbarnes00@gmail.com>

---
+++ LearningCPP/Lecture 30 - Headers and Prototypes/include/pythagoreanTheorem.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef PYTHAGOREANTHEOREM_H_
+#define PYTHAGOREANTHEOREM_H_
+
+#include <math.h> // pow
+
+double pythagoreanTheorem(double a, double b);
+
+#endif // !PYTHAGOREANTHEOREM_H_
+
+
---
+++ LearningCPP/Lecture 30 - Headers and Prototypes/src/pythagoreanTheorem.cpp
@@ -0,0 +1,7 @@
+#include "../include/pythagoreanTheorem.h"
+
+double pythagoreanTheorem(double a, double b){
+	double c;
+	c = pow(a, 2) + pow(b, 2);
+	return c;
+}

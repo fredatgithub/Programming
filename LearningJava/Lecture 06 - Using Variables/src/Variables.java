b87eb2d6783b1c8ce58d4af4d00356ce431d3abf
BlakeBarnes00 <ellisbarnes00@gmail.com>
2016-05-15 21:40:17 -0300

    LearningJava restarted

---
+++ LearningJava/Lecture 06 - Using Variables/src/Variables.java
@@ -0,0 +1,23 @@
+/**
+ * Created by ellis on 5/15/2016.
+ */
+public class Variables {
+    public static void main(String[] args){
+        int myInt = 1206021;
+        short myShort = 887;
+        long myLong = 321657487;
+        double myDouble = 7.643814;
+        float myFloat = 267.33f;
+        char myChar = 'e';
+        boolean myBool = true;
+
+        System.out.println("myInt = " + myInt);
+        System.out.println("myShort = " + myShort);
+        System.out.println("myLong = " + myLong);
+        System.out.println("myDouble = " + myDouble);
+        System.out.println("myFloat = " + myFloat);
+        System.out.println("myChar = " + myChar);
+        System.out.println("myBool = " + myBool);
+
+    }
+}
---
+++ LearningJava/Lecture 07 - Strings/src/Application.java
@@ -0,0 +1,10 @@
+/**
+ * Created by ellis on 5/15/2016.
+ */
+public class Application {
+    public static void main(String[] args){
+        String myString = "Hello";
+
+        System.out.println("myString: " + myString);
+    }
+}

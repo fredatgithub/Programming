94ae1ae0e0b7e43d13c1c2b4a01e15d7f5613f74
BlakeBarnes00 <ellisbarnes00@gmail.com>
2016-07-01 18:55:53 -0300

    Started reading a book instead of using online resources :+1:

--- LearningCPP/Lecture 30 - Headers and Prototypes/src/pythagoreanTheorem.cpp
+++
@@ -1,7 +0,0 @@
-#include "../include/pythagoreanTheorem.h"
-
-double pythagoreanTheorem(double a, double b){
-	double c;
-	c = pow(a, 2) + pow(b, 2);
-	return c;
-}
--- LearningCPP/Lecture 31 - Classes/src/cat.cpp
+++
@@ -1,9 +0,0 @@
-#include "../include/cat.h"
-
-void Cat::speak() {
-	std::cout << "Meowwwwww!" << std::endl;
-}
-
-void Cat::purr() {
-	std::cout << "Purrrrrr" << std::endl;
-}

1049f2ee8c44066d8b54c0ea21145c03b68379d9
BlakeBarnes00 <ellisbarnes00@gmail.com>
2016-07-01 22:50:26 -0300

    Chapter 1

---
+++ LearningCPP/Chapter 2/Exercises/ex1/main.cpp
@@ -0,0 +1,8 @@
+#include "../std_lib_facilities.h"
+
+int main() {
+    std::cout << "Hello, programming!\n";
+    std::cout << "Here we go!\n";
+
+    return 0;
+}
---
+++ LearningCPP/Chapter 2/Exercises/ex2/main.cpp
@@ -0,0 +1,11 @@
+#include "../std_lib_facilities.h"
+
+int main() {
+    std::cout << "To find the upstairs bathroom: \n";
+
+    std::cout << "Walk up the steps, there are 19 of them.\n";
+    std::cout << "Take a right once you go up them and walk past the first three doors on the left.\n";
+    std::cout << "The last door will be the bathroom, please flush the toilet.\n";
+
+    return 0;
+}

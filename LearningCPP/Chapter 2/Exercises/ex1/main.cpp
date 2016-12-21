07d12ac765d61ebe77fdb990aa76e8ef47fbf89c
Ellis Barnes <ellisbarnes00@gmail.com>
2016-12-21 02:35:36 -0500

    Using different resources to learn now

--- LearningCPP/Chapter 2/Exercises/ex1/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, programming!\n";
-    std::cout << "Here we go!\n";
-
-    std::cin.get(); // This is used so the console does't just close instantly.
-    return 0;
-}

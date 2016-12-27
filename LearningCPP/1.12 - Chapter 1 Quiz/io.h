500ba390808798ea049131a2e7220d6881dc7d20
Ellis Barnes <ellisbarnes00@gmail.com>
2016-12-27 00:33:12 -0500

    This was a fail, learning off of Git, get ready ;)

--- LearningCPP/1.12 - Chapter 1 Quiz/io.h
+++
@@ -1,7 +0,0 @@
-#ifndef IO_H
-#define IO_H
-
-int read_number();
-int write_answer(int a, int b);
-
-#endif
--- LearningCPP/1.9 - Header Files/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "add.h"
-
-int main() {
-  int answer = add(1, 2);
-  std::cout << "Answer: " << answer << "\n";
-  return 0;
-}
